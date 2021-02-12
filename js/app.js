5a94e46ad689d3cac15af72a7849e879ed76bd27
Jakub Stiburek <alcolp96@gmail.com>
2021-02-12 11:07:22 +0100

    Zmena barvy funguje. TODO opravit zmenu barvy tak, aby se zmenily vsechny karty bez nutnosti alespon jednou karty otocit

--- js/app.js
+++ js/app.js
@@ -2,11 +2,68 @@ import {Card} from "./Card.js"
 import {exampleCards} from "./exampleCards.js";
 
 //
+// Color picker
+//
+
+// Set of available color combinations
+const colorCombinations = [
+    ["#A17436", "#3DB841"],
+    ["#DAEDC9", "#DB94D2"],
+    ["#3A3399", "#D5B981"],
+    ["#AEB43C", "#DAE6B3"]
+]
+
+//  Get elements representing color combinations for User
+const pair1 = document.querySelector("#variant-1-a");
+const pair2 = document.querySelector("#variant-2-a");
+const pair3 = document.querySelector("#variant-3-a");
+const pairInitial = document.querySelector("#initial-variant-a");
+
+// Array to be filled according to user choice
+// Initially filled with original color combination
+let colors = ["#AEB43C", "#DAE6B3"];
+
+// Change all cards color according to what side is currently visible
+const changeVisibleSideColor = (colorCombo) => {
+    cards.forEach(card => {
+        if(card.classList.contains(".front")){
+            card.style.backgroundColor = colorCombo[0];
+        }
+        else{
+            card.style.backgroundColor = colorCombo[1];
+        }
+    })
+}
+
+// User picks a color combination
+const colorPicker = () => {
+    // Listen to user choosing the color combination
+    pair1.addEventListener("click", () => {
+        changeVisibleSideColor(colorCombinations[0]);
+        colors = colorCombinations[0];
+    })
+    pair2.addEventListener("click", () => {
+        colors = colorCombinations[1];
+        changeVisibleSideColor(colorCombinations[1]);
+    })
+    pair3.addEventListener("click", () => {
+        colors = colorCombinations[2];
+        changeVisibleSideColor(colorCombinations[2]);
+    })
+    pairInitial.addEventListener("click", () => {
+        colors = colorCombinations[3];
+        changeVisibleSideColor(colorCombinations[3]);
+    })
+    return colors;
+}
+
+//
 // Make 9 example flashcards.
 //
 
 // Generate n number of card elements
 // Card consists of div and p elements
+
 const n = 8;
 for(let i=0; i < n; i++){
     document.querySelector("#example-fc-wrapper").insertAdjacentHTML("afterbegin", "<div class='card'><p class='cardText front'></p></div>");
@@ -20,13 +77,15 @@ let cardObjects = [];
 
 // Fill cardObjects array with class Card instances
 for(let i=0; i < n; i++){
-    cardObjects[i] = new Card(exampleCards[i], cards[i]);
+    cardObjects[i] = new Card(exampleCards[i], cards[i], colors);
 }
 
 // Add event listener on click event for card within each class Card instance
 // Then add event listener on contextmenu event for card within each class Card instance
 cardObjects.forEach(cardObject => {
     cardObject.card.addEventListener("click", () => {
+        const colors = colorPicker();
+        cardObject.changeColors(colors);
         cardObject.leftClick();
     })
     cardObject.card.addEventListener("contextmenu", event => {
@@ -88,3 +147,6 @@ document.querySelector("#custom-fc-input").addEventListener("submit", event => {
     input.front.value = "";
     input.back.value = "";
 });
+
+
+
